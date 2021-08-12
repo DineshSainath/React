68b263de765df7fffbf4c2cdb0f871b53c30665e
Dinesh Sainath <sainath.dinesh@gmail.com>
2021-08-12 17:22:23 -0400

    mapping

---
+++ 16. mapping/src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+
+function createCard(c) {
+  return (
+    <Card
+      key={c.id}
+      name={c.name}
+      img={c.imgURL}
+      tel={c.phone}
+      email={c.email}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      {contacts.map(createCard)}
+    </div>
+  );
+}
+
+export default App;
