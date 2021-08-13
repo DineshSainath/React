34c12d1640d18355c3bced8b01967c18a0d960ec
Dinesh Sainath <sainath.dinesh@gmail.com>
2021-08-12 21:30:36 -0400

    arrow functions

---
+++ 18. arrow functions/src/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// var numbers = [3, 56, 2, 48, 5];
+
+////Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map(function (x) {
+//   return x * 2;
+// });
+
+// const newNumbers = numbers.map((x) => x * 2);
+// console.log(newNumbers);
+
+//////Filter - Create a new array by keeping the items that return true.
+// const newNumbers = numbers.filter(function(num) {
+//   return num < 10;
+// });
+
+// const newFilter = numbers.filter((x) => x < 10);
+// console.log(newFilter);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+//     return accumulator + currentNumber;
+// })
+
+// const newReduce = numbers.reduce((x, y) => x + y);
+// console.log(newReduce);
+
+////Find - find the first item that matches from an array.
+// const newNumber = numbers.find(function (num) {
+//   return num > 10;
+// })
+
+// const newFind = numbers.find((x) => x > 10);
+// console.log(newFind);
+
+////FindIndex - find the index of the first item that matches.
+// const newNumber = numbers.findIndex(function (num) {
+//   return num > 10;
+// })
+
+// const newIndex = numbers.findIndex((x) => x > 10);
+// console.log(newIndex);
